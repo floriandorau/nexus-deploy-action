fc5bd63ddc2ac9aba3b6e194defb434796a67cf1
Florian Dorau <43784665+floriandorau@users.noreply.github.com>
2019-03-18 14:02:08 +0100

    Make MVN deploy working (#1)
    * Make it deployable
    
    * Update README.md

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM maven:3.6.0-slim
 
 LABEL "com.github.actions.name"="nexus-deploy-action"
-LABEL "com.github.actions.description"="Deploy an artificat to Nexus using Maven deploy plugin"
+LABEL "com.github.actions.description"="Deploy artifacts to Nexus using Maven deploy plugin"
 LABEL "com.github.actions.icon"="box"
 LABEL "com.github.actions.color"="blue"
+
+ADD settings.xml /settings.xml
 
 ADD entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,29 +3,32 @@
 # Maven propertiees
 groupId=$GROUP_ID
 artifactId=$ARTIFACT_ID
+repositoryId=$REPOSITORY_ID
 version=$VERSION
-packaging=$PACKAGING
+packaging=zip
 
 # Neuxs propertiees
 nexus_user=$NEXUS_USER
 nexus_pw=$NEXUS_PW
 nexus_base=$NEXUS
-nexus_repo=$NEXUS_REPO
 
-workspace=$GITHUB_WORKSPACE
-
+# Artifact
 artifact = $GITHUB_WORKSPACE/$ARTIFACT
 
-nexus_url="https://$nexus_base/repository/$nexus_repo/"
+nexus_url="https://$nexus_base/repository/$repositoryId"
 
-echo "Deploying artifact '$artifact' to '$nexus_url'"
-mvn deploy:deploy-file \
+echo "Deploying artifact '$artifact' to '$nexus_url' as $groupId:$artifactId:$version"
+mvn --settings settings.xml deploy:deploy-file \
     -Durl=$nexus_url \
     -Dfile=$artifact \
     -DgroupId=$groupId \
+    -DrepositoryId=$repositoryId \
     -DartifactId=$artifactId \
     -Dversion=$version \
     -Dpackaging=$packaging \
     -DgeneratePom=true \
+    -Drepo.id=$repositoryId \
+    -Drepo.username=$nexus_user \
+    -Drepo.password=$nexus_pw
 
 echo "'$artifact' deployed to '$nexus_url'"
