e9b6f201cd6c59d57a873a3e927b5dafb7457a01
Florian Dorau <fdorau@it-economics.de>
2019-03-18 15:24:38 +0100

    Fix maven deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,11 +13,13 @@ nexus_pw=$NEXUS_PW
 nexus_base=$NEXUS
 
 # Artifact
-artifact = $GITHUB_WORKSPACE/$ARTIFACT
+artifact = "$GITHUB_WORKSPACE/$ARTIFACT"
 
 nexus_url="https://$nexus_base/repository/$repositoryId"
 
-echo "Deploying artifact '$artifact' to '$nexus_url' as $groupId:$artifactId:$version"
+ls -la
+
+echo "Deploying artifact '$artifact' to '$nexus_url' as '$groupId:$artifactId:$version'"
 mvn --settings settings.xml deploy:deploy-file \
     -Durl=$nexus_url \
     -Dfile=$artifact \
